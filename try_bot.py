523bbc32ad8920a1839010572b016c1543df73af
Amit Hirpara <hirparaamit43@gmail.com>
2024-02-05 20:38:29 +0530

    Update Code

--- try_bot.py
+++ try_bot.py
@@ -1,18 +1,20 @@
 import google.generativeai as genai
-import os
+import os, requests
+from bs4 import BeautifulSoup
+from langchain_community.document_loaders import WebBaseLoader
+
+url = 'https://www.apple.com/in/macbook-pro/'
+loader = WebBaseLoader(url)
+data = loader.load()
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel('gemini-pro')
-
-text_file_path = './output.txt'
-with open(text_file_path, 'r', encoding='utf-8') as file:
-    context = file.read()
 
 while True:
     question = input("Enter your query: ")
     prompt = f"""Answer the question as precise as possible using the provided context. If the answer is
                     not contained in the context, say "answer not available in context" \n\n
-                    Context: \n {context}?\n
+                    Context: \n {data[0].page_content}?\n
                     Question: \n {question} \n
                     Answer:
                 """
