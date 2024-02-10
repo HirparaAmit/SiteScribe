cb06d9766ef3a6001504106694cb9c4266547a45
Amit Hirpara <hirparaamit43@gmail.com>
2024-02-10 17:08:31 +0530

    Update prompt

--- bot.py
+++ bot.py
@@ -2,17 +2,18 @@ import google.generativeai as genai
 import os
 from langchain_community.document_loaders import WebBaseLoader
 
-url = 'https://www.amazon.com/A315-24P-R7VH-Display-Quad-Core-Processor-Graphics/dp/B0BS4BP8FB/ref=sr_1_9?keywords=laptop&sr=8-9'
+url = 'https://arxiv.org/abs/2402.05935'
 loader = WebBaseLoader(url)
 data = loader.load()
+print(data[0].page_content)
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel('gemini-pro')
 
 while True:
     question = input("Enter your query: ")
-    prompt = f"""Answer the question as precise as possible using the provided context. If the answer is
-                    not contained in the context, say "answer not available in context" \n\n
+    prompt = f"""Answer the question as precise as possible using the provided context. This context is scraped from a website, so if user mentions website, he means this given context.
+                    Only return the helpful answer below and nothing else.\n\n
                     Context: \n {data[0].page_content}?\n
                     Question: \n {question} \n
                     Answer:
