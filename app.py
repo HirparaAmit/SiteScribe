5361c3807565a0766e50b1e7fb1757d7d85f4f84
Amit Hirpara <hirparaamit43@gmail.com>
2024-02-25 14:11:34 +0530

    Update UI

--- app.py
+++ app.py
@@ -5,25 +5,25 @@ from langchain_community.document_loaders import WebBaseLoader
 
 app = Flask(__name__)
 
-url = 'https://arxiv.org/abs/2402.05935'
-loader = WebBaseLoader(url)
-data = loader.load()
+# url = 'https://arxiv.org/abs/2402.05935'
+# loader = WebBaseLoader(url)
+# data = loader.load()
 
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
-model = genai.GenerativeModel('gemini-pro')
+# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# model = genai.GenerativeModel('gemini-pro')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/get', methods=['GET','POST'])
-def chat():
-    if request.method == 'POST':
-        msg = request.form['msg']
-        prompt = f"""Answer the question as precise as possible using the provided context. This context is scraped from a website, so if user mentions website, he means this given context.\n\n
-                    Context: \n {data[0].page_content}?\n
-                    Question: \n {msg} \n
-                    Answer:
-                """
-        response = model.generate_content(prompt)
-        return response.text
+# @app.route('/get', methods=['GET','POST'])
+# def chat():
+#     if request.method == 'POST':
+#         msg = request.form['msg']
+#         prompt = f"""Answer the question as precise as possible using the provided context. This context is scraped from a website, so if user mentions website, he means this given context.\n\n
+#                     Context: \n {data[0].page_content}?\n
+#                     Question: \n {msg} \n
+#                     Answer:
+#                 """
+#         response = model.generate_content(prompt)
+#         return response.text
