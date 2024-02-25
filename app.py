afbcd7ef7642037b602a60202fd6e656aab95291
Amit Hirpara <hirparaamit43@gmail.com>
2024-02-25 13:07:31 +0530

    Update code

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request
+import google.generativeai as genai
+import os
+from langchain_community.document_loaders import WebBaseLoader
 
 app = Flask(__name__)
+
+url = 'https://arxiv.org/abs/2402.05935'
+loader = WebBaseLoader(url)
+data = loader.load()
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+model = genai.GenerativeModel('gemini-pro')
 
 @app.route('/')
 def index():
@@ -10,4 +20,10 @@ def index():
 def chat():
     if request.method == 'POST':
         msg = request.form['msg']
-        return f"This is your input message: {msg}"
+        prompt = f"""Answer the question as precise as possible using the provided context. This context is scraped from a website, so if user mentions website, he means this given context.\n\n
+                    Context: \n {data[0].page_content}?\n
+                    Question: \n {msg} \n
+                    Answer:
+                """
+        response = model.generate_content(prompt)
+        return response.text
